b83b6b93d8cfc736479fd7d0b21f1b6c6bad2f8f
Caique Moraes <ike.moraes93@gmail.com>
2024-11-10 10:10:46 -0300

    chore: remove CommonJS module and add ES module support with updated exports

---
+++ dist/index.mjs
@@ -0,0 +1,1 @@
+var R=Object.defineProperty;var o=(t,e)=>{for(var L in e)R(t,L,{get:e[L],enumerable:!0})};var i={};o(i,{Left:()=>r,Right:()=>s,left:()=>h,right:()=>n});var r=class{constructor(e){this.value=e}isLeft(){return!0}isRight(){return!1}},s=class{constructor(e){this.value=e}isLeft(){return!1}isRight(){return!0}},h=t=>new r(t),n=t=>new s(t);export{i as Either};
